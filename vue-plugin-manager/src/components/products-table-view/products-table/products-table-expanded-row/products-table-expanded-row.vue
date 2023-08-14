80903fb1e32624d9470e6fd708814e4fde82019e
isaacsoto <isaacsotomar@gmail.com>
2023-08-14 02:05:44 +0200

    Expaded row styles updated

--- vue-plugin-manager/src/components/products-table-view/products-table/products-table-expanded-row/products-table-expanded-row.vue
+++ vue-plugin-manager/src/components/products-table-view/products-table/products-table-expanded-row/products-table-expanded-row.vue
@@ -1,29 +1,37 @@
 <template>
-    <tr class="big-row">
-      <td :colspan="columns.length">
-        <img :src="`${item.raw?.images['240w']}`">
-        <v-card variant="tonal" class="table-item-card">
-          <v-card-title>{{ item.raw?.name }}</v-card-title>
-          <v-card-text>{{ item.raw?.description }}</v-card-text>
+  <tr>
+    <td :colspan="columns.length">
+      <div class="expanded-row">
+        <v-img :src="`${item.raw?.images['480w']}`" class="expanded-image"></v-img>
+        <v-card variant="outlined" class="table-item-card">
+          <div class="expanded-info">
+            <v-card-text class="feature-highlight">{{ item.raw?.featureHighlight.toUpperCase() }}</v-card-text>
+            <v-card-title>{{ item.raw?.name }}</v-card-title>
+            <v-card-subtitle class="description">{{ item.raw?.description }}</v-card-subtitle>
+            <v-card-actions>
+              <v-btn text color="primary" @click="openExternalURL">WEBSITE</v-btn>
+            </v-card-actions>
+          </div>
         </v-card>
-        <v-card variant="tonal" class="table-item-card">
-          <v-card-item>{{ item.raw?.installers?.win64?.version }}</v-card-item>
-          <v-card-item>{{ item.raw?.installers?.macOs?.version }}</v-card-item>
-        </v-card>
-      </td>
-    </tr>
-  </template>
-  
-  <script>
-  export default {
-    props: {
-      columns: Array,
-      item: Object,
+      </div>
+    </td>
+  </tr>
+</template>
+
+<script>
+export default {
+  props: {
+    columns: Array,
+    item: Object,
+  },
+  methods: {
+    openExternalURL() {
+      window.open('https://www.softube.com' + this.item.raw?.url, '_blank');
     },
-  };
-  </script>
-  
-  <style scoped lang="scss">
-    @import './products-table-expanded-row.scss';
-  </style>
-  
+  },
+};
+</script>
+
+<style scoped lang="scss">
+  @import './products-table-expanded-row.scss';
+</style>
